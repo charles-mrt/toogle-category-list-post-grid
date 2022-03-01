9f535d80d0eb6815f517af16c5a04211064e44f3
Charles Martins <60299718+charles-mrt@users.noreply.github.com>
2022-03-01 12:36:11 -0300

    Create functions.php
    enqueue script by page ID - first commit

---
+++ wp-content/theme-name/functions.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+* Enqueue scripts by page ID.	
+* 
+* @return void
+*/
+
+function enqueueCustomScripts() {
+	
+	/*
+	* This page is only accessible by mobile device
+	*/
+	define('MOBILE_FRONT_PAGE_ID' , 1938 );
+
+	if ( is_page( MOBILE_FRONT_PAGE_ID ) ) {
+        
+		// js scripts
+		wp_enqueue_script ( 
+            'scriptToogleCategoryFilterList' ,  
+            get_stylesheet_directory_uri() . 
+            '/customScripts/main.js', 
+            array() , false , true 
+        );	
+
+		// css scripts
+		wp_enqueue_style( 
+            'styleToogleShowCategoryFilterList', 
+            get_stylesheet_directory_uri() . 
+            '/customScripts/style.css'
+        );
+	}
+}
+ add_action( 'wp_enqueue_scripts', 'enqueueCustomScripts' );
+
+?>
